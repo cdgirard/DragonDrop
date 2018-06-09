b5770bd5527162b0f96dc539bad65c4ada0eab7f
cdgira <cdgira@cs.ship.edu>
2018-06-09 00:06:44 -0400

    Removed old drag code.

--- core/src/com/mm/screen/input/GameScreenInputAdapter.java
+++ core/src/com/mm/screen/input/GameScreenInputAdapter.java
@@ -1,8 +1,6 @@
 package com.mm.screen.input;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.utils.Disposable;
 import com.mm.objects.Dragon;
 import com.mm.screen.GameScreen;
@@ -14,10 +12,6 @@ public class GameScreenInputAdapter extends InputAdapter implements Disposable
     public GameScreenInputAdapter(GameScreen gs)
     {
 	m_screen = gs;
-	//InputMultiplexer im = (InputMultiplexer)Gdx.input.getInputProcessor();
-	//im.addProcessor(this);
-	//Gdx.input.setInputProcessor(this);
-
     }
     
     /**
@@ -26,17 +20,6 @@ public class GameScreenInputAdapter extends InputAdapter implements Disposable
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer)
     {
-
-	Dragon hero = Dragon.getInstance();
-        System.out.println("Screen: "+screenX+" : "+screenY);
-        System.out.println("Hero: "+hero.getX()+" : "+hero.getY());
-	if ((screenX >= hero.getX()-50) && (screenX <= hero.getX()+50))
-	{
-	    if ((screenY >= hero.getY()-50) && (screenY <= hero.getY()+50))
-	    {
-		hero.setPosition(screenX, screenY);
-	    }
-	}
 	return true;
     }
     
@@ -46,16 +29,6 @@ public class GameScreenInputAdapter extends InputAdapter implements Disposable
     @Override
     public boolean touchDown (int screenX, int screenY, int pointer, int button)
     {
-	//m_screen.updateMessageLabel("Why would I move there?");
-	Dragon hero = Dragon.getInstance();
-	//hero.setPosition(hero.getX(), hero.getY());  // Stop the hero moving.
-	if ((screenX >= hero.getX()-50) && (screenX <= hero.getX()+50))
-	{
-	    if ((screenY >= hero.getY()-50) && (screenY <= hero.getY()+50))
-	    {
-		hero.setPosition(screenX, screenY);
-	    }
-	}
 	return false;
     }
     
@@ -65,7 +38,6 @@ public class GameScreenInputAdapter extends InputAdapter implements Disposable
     @Override
     public boolean touchUp (int screenX, int screenY, int pointer, int button)
     {
-	m_screen.updateMessageLabel("Why would I move there?");
 	return false;
     }
 
