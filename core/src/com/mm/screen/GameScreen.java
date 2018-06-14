d345bc3bd909d629ce2b6b9cd3d067f7ae866afb
cdgira <cdgira@cs.ship.edu>
2018-06-13 22:40:29 -0400

    Dragon now drops with released.
    Managed using Box2d.

--- core/src/com/mm/screen/GameScreen.java
+++ core/src/com/mm/screen/GameScreen.java
@@ -13,6 +13,12 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
@@ -20,8 +26,12 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
 import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Array;
 import com.mm.helpers.Assets;
+import com.mm.helpers.CollisionHandler;
 import com.mm.helpers.UIHelper;
+import com.mm.objects.AbstractGameObject;
+import com.mm.objects.DroppingDragon;
 import com.mm.objects.Dragon;
 import com.mm.objects.DragonSlot;
 import com.mm.screen.input.GameScreenInputAdapter;
@@ -36,6 +46,8 @@ public class GameScreen extends SizableScreen
     RayHandler rayHandler;
     Light light;
     World world;
+    
+    public Array<AbstractGameObject> m_droppedDragons;
     
     private Skin uiSkin = new Skin(Gdx.files.internal("uiskin_copy.json"));
     private Stage m_stage;
@@ -54,11 +66,18 @@ public class GameScreen extends SizableScreen
     
     private float m_runTime = 0f;
     
+ // For Box2D Debugging
+ 	private static final boolean DEBUG_DRAW_BOX2D_WORLD = true;
+ 	private Box2DDebugRenderer b2DebugRenderer;
+
+    
     public GameScreen()
     {
         m_cam = new OrthographicCamera();
         
         m_background = Assets.assetManager.get(Assets.GAME_SCREEN,Texture.class);
+        
+        m_droppedDragons = new Array<AbstractGameObject>();
         
         preferredWidth = m_background.getWidth();
         preferredHeight = m_background.getHeight();
@@ -87,7 +106,14 @@ public class GameScreen extends SizableScreen
         // Move Camera to 0,0
         //cam.translate(-cam.position.x, -cam.position.y, 0);
         
-        world = new World(new Vector2(0,0),true);
+ 
+	if (world != null)
+	    world.dispose();
+	world = new World(new Vector2(0, 9.81f), true);
+	world.setContactListener(new CollisionHandler(this));
+
+        
+        //world = new World(new Vector2(0,0),true);
         
         RayHandler.setGammaCorrection(true);
         RayHandler.useDiffuseLight(true);
@@ -103,6 +129,8 @@ public class GameScreen extends SizableScreen
             slots[index] = new DragonSlot(new Vector2(0,m_quitButton.getHeight()));
         }
         slots[0].setDragon(Assets.assetManager.get(Assets.GOTH_DRAGON,Texture.class));
+        
+        b2DebugRenderer = new Box2DDebugRenderer();
     }
     
     public DragonSlot getSlot(int x, int y)
@@ -120,6 +148,34 @@ public class GameScreen extends SizableScreen
 	}
 	return null;
     }
+    
+    public void dropDragon(Vector2 pos)
+    {
+	DroppingDragon droppedDragon = new DroppingDragon();
+	
+	BodyDef bodyDef = new BodyDef();
+	bodyDef.position.set(pos);
+	bodyDef.angle = 0; // rotation;
+	Body body = world.createBody(bodyDef);
+	body.setType(BodyType.DynamicBody);
+	body.setUserData(droppedDragon);
+	droppedDragon.body = body;
+	
+	PolygonShape polygonShape = new PolygonShape();
+	float halfWidth = droppedDragon.bounds.width / 2.0f;
+	float halfHeight = droppedDragon.bounds.height / 2.0f;
+	polygonShape.setAsBox(halfWidth, halfHeight);
+	
+	FixtureDef fixtureDef = new FixtureDef();
+	fixtureDef.shape = polygonShape;
+	fixtureDef.density = 50;
+	fixtureDef.restitution = 0.5f;
+	fixtureDef.friction = 0.5f;
+	body.createFixture(fixtureDef);
+	polygonShape.dispose();
+	m_droppedDragons.add(droppedDragon);
+
+    }
 
     /**
      * TODO: Potential refactor as near duplicate with MainScreen
@@ -127,6 +183,12 @@ public class GameScreen extends SizableScreen
     @Override
     public void render(float delta)
     {
+	world.step(delta, 8, 3);
+        for (AbstractGameObject obj: m_droppedDragons)
+        {
+            obj.update(delta);
+        }
+	
         m_runTime += delta;
         Gdx.gl.glClearColor(1.0f, 0.0f, 0.0f, 1);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
@@ -142,6 +204,12 @@ public class GameScreen extends SizableScreen
         if (Dragon.getInstance().getActive())
             Dragon.getInstance().render(batcher,delta);
         // Create 3 slots for dragons to be dragged and dropped
+        
+        for (AbstractGameObject obj: m_droppedDragons)
+        {
+            obj.render(batcher);
+            updateMessageLabel("Dragon Dropping:" + obj.position.x+" : "+obj.position.y);
+        }
         
         int x = 0;
         for (DragonSlot slot : slots)
@@ -167,6 +235,12 @@ public class GameScreen extends SizableScreen
         m_stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         m_stage.draw();
      //   Gdx.app.log("MainScreen", "Stage: "+m_stage.getViewport().getScreenHeight()+" , "+m_stage.getViewport().getScreenWidth());
+
+        
+	if (DEBUG_DRAW_BOX2D_WORLD)
+	{
+		b2DebugRenderer.render(world, m_cam.combined);
+	}
 
     }
 
