db34f2c025f1002b0f90ffdd89bc3120e11d9128
cdgira <cdgira@cs.ship.edu>
2018-06-15 23:09:16 -0400

    Buy button added, pops up an unfinished window.

--- core/src/com/mm/screen/GameScreen.java
+++ core/src/com/mm/screen/GameScreen.java
@@ -86,6 +86,7 @@ public class GameScreen extends SizableScreen
     private Label goldLabel;
 
     private ImageButton m_quitButton;
+    
 
     private DragonSlot[] slots = new DragonSlot[5];
 
@@ -94,14 +95,14 @@ public class GameScreen extends SizableScreen
     private GameScreenInputAdapter inputProcessor;
 
     private float m_runTime = 0f;
+    
+	// Buy Dragon
+	private Window winBuyDragon;
+	private ImageButton btnBuyGothDragon;
+	private ImageButton btnBuyHazyDragon;
+	private TextButton btnWinOptCancel;
 
-    // Buy Dragon
-    public Window winBuyDragon;
-    private ImageButton btnBuyGothDragon;
-    private ImageButton btnBuyHazyDragon;
     private ImageButton btnBuyBookDragon;
-    public TextButton btnWinOptCancel;
-
     // For Box2D Debugging
     private static final boolean DEBUG_DRAW_BOX2D_WORLD = true;
     private Box2DDebugRenderer b2DebugRenderer;
@@ -293,6 +294,126 @@ public class GameScreen extends SizableScreen
 	m_paused = true;
 	winBuyDragon.setVisible(true);
     }
+    
+    private Table buildBuyDragonsWindowLayer()
+    {
+	winBuyDragon = new Window("Options", uiSkin);
+	winBuyDragon.add(buildBuyDragonsRow()).row();
+	winBuyDragon.add(buildOptWinSkinSelection()).row();
+	winBuyDragon.add(buildOptWinDebug()).row();
+	winBuyDragon.add(buildOptWinButtons()).pad(10, 0, 10, 0);
+	// Making the whole window transparent.
+	winBuyDragon.setColor(1, 1, 1, 0.8f);
+	winBuyDragon.setVisible(false);
+	// winBuyDragon.debug();
+	winBuyDragon.pack();
+	// Not doing anything
+	winBuyDragon.setPosition(preferredWidth - winBuyDragon.getWidth() - 50, 50);
+	winBuyDragon.setMovable(false);
+	return winBuyDragon;
+    }
+	
+	private Table buildBuyDragonsRow()
+	{
+		Table tbl = new Table();
+		tbl.pad(10, 10, 0, 10);
+		tbl.add(new Label("Goth Dragon", uiSkin, "default-font", Color.ORANGE)).colspan(3);
+		tbl.row();
+		tbl.columnDefaults(0).padRight(10);
+		tbl.columnDefaults(1).padRight(10);
+		UIHelper.addTexture(Assets.GOTH_DRAGON, Assets.assetManager.get(Assets.GOTH_DRAGON, Texture.class));
+		btnBuyGothDragon = UIHelper.constructButton(Assets.GOTH_DRAGON,Assets.GOTH_DRAGON);
+		tbl.add(btnBuyGothDragon);
+		UIHelper.addTexture(Assets.HAZY_DRAGON, Assets.assetManager.get(Assets.HAZY_DRAGON, Texture.class));
+		btnBuyHazyDragon = UIHelper.constructButton(Assets.HAZY_DRAGON,Assets.HAZY_DRAGON);
+		tbl.add(btnBuyHazyDragon);
+//		sldSound = new Slider(0.0f, 1.0f, 0.1f, false, skinLibgdx);
+//		tbl.add(sldSound);
+//		tbl.row();
+//		chkMusic = new CheckBox("", skinLibgdx);
+//		tbl.add(chkMusic);
+//		tbl.add(new Label("Music", skinLibgdx));
+//		sldMusic = new Slider(0.0f, 1.0f, 0.1f, false, skinLibgdx);
+//		tbl.add(sldMusic);
+//		tbl.row();
+		return tbl;
+	}
+	
+	private Table buildOptWinSkinSelection()
+	{
+		Table tbl = new Table();
+		tbl.pad(10, 10, 0, 10);
+		tbl.add(new Label("Character Skin", uiSkin, "default-font", Color.ORANGE)).colspan(2);
+//		tbl.row();
+//		selCharSkin = new SelectBox<CharacterSkin>(skinLibgdx);
+//		selCharSkin.setItems(CharacterSkin.values());
+//		selCharSkin.addListener(new ChangeListener() {
+//			@Override
+//			public void changed(ChangeEvent event, Actor actor) 
+//			{
+//				onCharSkinSelected(((SelectBox<CharacterSkin>)actor).getSelectedIndex());
+//			}
+//		});
+//		tbl.add(selCharSkin).width(120).padRight(20);
+//		imgCharSkin = new Image(Assets.instance.block.block);
+//		tbl.add(imgCharSkin).width(80).height(50);
+		return tbl;
+	}
+	
+	private Table buildOptWinDebug()
+	{
+		Table tbl = new Table();
+		tbl.pad(10, 10, 0, 10);
+		tbl.add(new Label("Debug", uiSkin, "default-font", Color.RED)).colspan(3);
+		tbl.row();
+		tbl.columnDefaults(0).padRight(10);
+		tbl.columnDefaults(1).padRight(10);
+		tbl.add(new Label("Show FPS Counter", uiSkin));
+		tbl.row();
+		return tbl;
+	}
+	
+	private Table buildOptWinButtons()
+	{
+		Table tbl = new Table();
+		Label lbl = null;
+		lbl = new Label("", uiSkin);
+//		lbl.setColor(0.75f, 0.75f, 0.75f, 1);
+//		lbl.setStyle(new LabelStyle(lbl.getStyle()));
+//		lbl.getStyle().background = skinLibgdx.newDrawable("white");
+//		tbl.add(lbl).colspan(2).height(1).width(220).pad(0, 0, 0, 1);
+//		tbl.row();
+//		lbl = new Label("", skinLibgdx);
+//		lbl.setColor(0.5f, 0.5f, 0.5f, 1);
+//		lbl.setStyle(new LabelStyle(lbl.getStyle()));
+//		lbl.getStyle().background = skinLibgdx.newDrawable("white");
+//		tbl.add(lbl).colspan(2).height(1).width(220).pad(0, 1, 5, 0);
+//		tbl.row();
+//		btnWinOptSave = new TextButton("Save", skinLibgdx);
+//		tbl.add(btnWinOptSave).padRight(30);
+//		btnWinOptSave.addListener(new ChangeListener() {
+//			@Override
+//			public void changed(ChangeEvent event, Actor actor) {
+//				onSaveClicked();
+//			}
+//		});
+//		btnWinOptCancel = new TextButton("Cancel", skinLibgdx);
+//		tbl.add(btnWinOptCancel);
+//		btnWinOptCancel.addListener(new ChangeListener() {
+//			@Override
+//			public void changed(ChangeEvent event, Actor actor) {
+//				onCancelClicked();
+//			}
+//		});
+		return tbl;
+	}
+
+	
+	public void onOptionsClicked()
+	{
+		winBuyDragon.setVisible(true);
+	}
+
 
     public DragonSlot getSlot(int x, int y)
     {
