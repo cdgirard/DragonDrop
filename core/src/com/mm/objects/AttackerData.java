0c8762fbfa0b3864e8b67fdbfe62e8b2ace64976
cdgira <cdgira@cs.ship.edu>
2018-06-15 20:01:55 -0400

    Attackers have different speeds.

--- core/src/com/mm/objects/AttackerData.java
+++ core/src/com/mm/objects/AttackerData.java
@@ -6,10 +6,12 @@ public class AttackerData
 {
     public Texture m_image;
     public int m_gold = 10;
+    public float m_impulse = -6.0f;
     
-    public AttackerData(Texture img, int gold)
+    public AttackerData(Texture img, int gold, float impulse)
     {
 	m_image = img;
 	m_gold = gold;
+	m_impulse = impulse;
     }
 }
