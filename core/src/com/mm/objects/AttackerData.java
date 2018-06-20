f5b8cade21cd1596c515a3a4de3bac2b7030a26d
cdgira <cdgira@cs.ship.edu>
2018-06-19 21:07:07 -0400

    Attackers take different amounts of gold depending on type.

--- core/src/com/mm/objects/AttackerData.java
+++ core/src/com/mm/objects/AttackerData.java
@@ -5,15 +5,17 @@ import com.badlogic.gdx.graphics.Texture;
 public class AttackerData
 {
     public final Texture m_image;
-    public final int m_gold;
+    public final int m_goldValue;
     public final float m_impulse;
     public final float m_baseHealth;
+    public final int m_goldSteals;
     
-    public AttackerData(Texture img, int gold, float impulse, float health)
+    public AttackerData(Texture img, int gold, float impulse, float health, int goldSteals)
     {
 	m_image = img;
-	m_gold = gold;
+	m_goldValue = gold;
 	m_impulse = impulse;
 	m_baseHealth = health;
+	m_goldSteals = goldSteals;
     }
 }
